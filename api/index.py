f6fdd2cb78498d8c48c036279d789dd6feab54a2
Stephen Turner <stephen@littlebrainz.com>
2023-05-08 10:22:58 +0100

    Prettied

--- api/index.py
+++ api/index.py
@@ -1,10 +1,14 @@
+# api/index.py
+
+
 from http.server import BaseHTTPRequestHandler
+
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+        self.wfile.write(
+            'Hello, random Internet person!'.encode('utf-8'))
